7790c0f49773299d8c2b88ce33b14cfddc52b086
pjvalentini <pj.valentini@gmail.com>
2017-07-04 03:53:38 -0400

    javascript files updated

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Descending-String-Exersise
+
+Descending String Exercise assignment at NYCDA
+
+Create a string with asterisks in descending order from 10 to 1.
+This exercise was done in javascript and node in the terminal.
---
+++ desString.js
@@ -0,0 +1,26 @@
+// Descending String Exercise
+
+// let counter stores a function with a number passed in.
+// let asteriskArr stores an empty array that can be looped over.
+// the array will hold asterisks that will be iterated over.
+let counter = function(num) {
+	let asteriskArr = [];
+	for (var i = 0; i < num; i++) {
+		asteriskArr.push("*");
+	}
+
+// let int stores a function that sets the interval over the loop.
+// console.log and join the asterisks, seperating them with a space.
+// add a condition that if the length of the array is not equal to 1 then
+// take way 1 asterisk per line throughout the loop, until 1 asterisk is left.
+//
+	let int = setInterval(() => {
+		console.log(asteriskArr.join(" "));
+		if (asteriskArr.length !== 1) {
+			  asteriskArr.pop();
+		} else {
+			clearInterval(int);
+		}
+	}, 1000);
+};
+counter(10);
